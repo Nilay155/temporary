caef1ff634dc645930d365f8966ef998f794cd77
dino2580 <anshgoel2580@gmail.com>
2024-07-28 04:58:53 +0530

    njgf

--- api/src/gen-ai/gemini.js
+++ api/src/gen-ai/gemini.js
@@ -2,13 +2,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function gemini() {
+export default async function gemini(data) {
     const model = genAI.getGenerativeModel({ model: "gemini-flash-1.5" });
 
-    const prompt = ""
+    const prompt = "I want to get name,date of birth,gender and 16 digit number"
 
-    const result = await model.generateContent([prompt]);
+    const result = await model.generateContent([prompt,data]);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
+    return text
 }
